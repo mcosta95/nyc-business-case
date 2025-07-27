4508b8d3d3c42d3486ee862f4449bd032eedd83a
mcosta95 <95431729+mcosta95@users.noreply.github.com>
2025-07-27 23:13:14 +0100

    add latest changes

--- src/plots.py
+++ src/plots.py
@@ -1,5 +1,8 @@
 import seaborn as sns
+import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+
 
 def plot_box_plot(
     df, 
@@ -54,4 +57,64 @@ def plot_corr_heatmap(df, num_cols):
     corr = df[num_cols].corr()
     sns.heatmap(corr, annot=True, cmap='coolwarm')
     plt.title("Correlation Matrix")
+    plt.show()
+
+
+def plot_violation_code_analysis(violation_risk_dist, violation_critical_map):
+
+    plt.figure(figsize=(14,11))
+
+    bars = []
+    bottom = pd.Series([0] * len(violation_risk_dist), index=violation_risk_dist.index)
+
+    colors = {
+        'High': '#d73027',
+        'Moderate': '#fc8d59',
+        'Low': 'green'
+    }
+
+    # Draw stacked bars for each risk category
+    for risk in colors.keys():
+        if risk in violation_risk_dist.columns:
+            bar = plt.barh(
+                y=violation_risk_dist.index,
+                width=violation_risk_dist[risk],
+                left=bottom,
+                label=risk,
+                color=colors[risk]
+            )
+            bottom += violation_risk_dist[risk]
+            bars.append(bar)
+
+    # Draw red circles for critical violations
+    for i, vc in enumerate(violation_risk_dist.index):
+        if violation_critical_map.get(vc) == 'Critical':
+            plt.plot(-5, i, 'o', color='red')  # circle left of the bar
+
+    # Formatting
+    plt.title('Violation Codes with Risk Category Breakdown')
+    plt.xlabel('Inspection Count')
+    plt.ylabel('Violation Code')
+
+    # Custom legend to include red circle for critical
+    critical_legend = Line2D([0], [0], marker='o', color='w', label='Critical',
+                             markerfacecolor='red', markersize=8)
+    plt.legend(handles=[*bars, critical_legend], loc='center left', bbox_to_anchor=(1, 0.5), title='Risk Category')
+
+    plt.tight_layout()
+    plt.show()
+
+
+
+
+def stacked_bar_plot(df, col_a, col_b, label_a, title, legend_title="Risk Category"):
+
+    df_plot = pd.crosstab(df[col_a], df[col_b], normalize='index').sort_values(by='High', ascending=False)
+    df_plot.plot(kind='bar', stacked=True, figsize=(12,6), colormap='coolwarm')
+    plt.title(title)
+    plt.xlabel(label_a)
+    plt.ylabel("Proportion")
+    plt.xticks(rotation=45, ha='right')
+    plt.legend(title=legend_title)
+    plt.tight_layout()
     plt.show()
